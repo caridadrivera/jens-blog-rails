05e40b0b36f5f331c30418649194eb565ca5d606
Caridad Rivera <ccrive10@gmail.com>
2019-08-25 00:36:35 -0400

    models setup

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+require 'bcrypt'
+
+class User < ApplicationRecord
+
+  has_many :posts
+  has_many :friendships, class_name: "Friendship", foreign_key: "user_id"
+  has_many :friends, through: :friendships
+
+
+  has_secure_password
+
+  # validates_uniqueness_of :username
+  validates :username, presence: true
+  validates :password, presence: true
+
+end
